2e4763cc9b8d9a6874a5b8c4bbbf1bd0931b0c10
bogdanov.ap <bogdanov.ap@gmail.com>
2023-09-26 13:45:27 +0300

    HW-07

---
+++ 07/HW-07_01/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+template <typename T>
+void remove_duplicates(std::vector<T>& vec) {
+//    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+//    std::cout << "NOT sorted" << std::endl;
+//    for (const int& value : vec) {
+//        std::cout << value << " ";
+//    }
+
+//  std::unique - итератор на конец массива уникальных элементотв
+//  удаляет элементы идущие подряд. поэтому нужно отсортировать массив
+//  vec.erase - удаляеб элементы, начиная с итератора, который вернул std::unique
+    std::sort(vec.begin(), vec.end());
+//    for (const int& value : vec) {
+//        std::cout << value << " ";
+//    }
+    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+}
+
+int main() {
+    std::string input;
+    int num;
+
+    std::cout << "IN : ";
+    std::getline(std::cin, input);
+
+    std::istringstream iss(input);
+
+    std::vector<int> numbers;
+    while (iss >> num) {
+        numbers.push_back(num);
+    }
+
+    remove_duplicates(numbers);
+
+    std::cout << "OUT: ";
+    for (const int& value : numbers) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ 07/HW-07_02/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+template <typename Container>
+void print_container(const Container& container) {
+    auto element = container.begin();
+    while (true) {
+        std::cout << *element;
+        ++element;
+        if (element == container.end()) break;
+        std::cout << ", ";
+    }
+//    for (const auto& item : container) {
+//        std::cout << item;
+//        if (item != *container.end()) {
+//            std::cout << ", ";
+//        }
+//    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cout << "test_set: ";
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::cout << "test_list: ";
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::cout << "test_vector: ";
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    return 0;
+}
